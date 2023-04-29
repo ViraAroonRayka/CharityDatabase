fcee86216f03edb930c2d1954d100269dedcdae0
ViraAroonRayka <viraaroonrayka@gmail.com>
2023-04-29 19:31:34 +0430

    Add project files.

---
+++ CharityDatabase/Pages/Needee.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CharityDatabase.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        public void OnPost()
+        {
+            string firstName = Request.Form["FirstNameTextBox"];
+            string lastName = Request.Form["LastNameTextBox"];
+            string nationalCode = Request.Form["NationalCodeTextBox"];
+
+            ViewData["NeedeeInfo"] = firstName + " " + lastName + " کد ملی " + nationalCode;
+        }
+    }
+}
