1906d9119a08c8af204d94052c04ca8ee7ddf36a
ViraAroonRayka <viraaroonrayka@gmail.com>
2023-05-14 20:29:17 +0430

    Edit and search added

--- CharityDatabase/Pages/Needee/Edit.cshtml.cs
+++ CharityDatabase/Pages/Needee/Edit.cshtml.cs
@@ -14,27 +14,7 @@ namespace CharityDatabase.Pages.Needee
         public Db.Needee needeeToEdit;
         public EditModel()
         {
-            string id = Request.Query["Id"].ToString();
-            if (!string.IsNullOrEmpty(id))
-            {
-                try
-                {
-                    int idToEdit = Convert.ToInt32(id);
-                    needeeToEdit = _context.Needees.FirstOrDefault(n => n.Id == idToEdit);
-                    if (needeeToEdit == null)
-                    {
-                        ViewData["Error"] += "مددجویی با این شناسه یافت نشد";
-                    }
-                }
-                catch (Exception ex)
-                {
-                    ViewData["Error"] += "عملیات ویرایش با مشکل مواجه شد\n" + ex.Message;
-                }
-                finally
-                {
-                    _context.Dispose();
-                }
-            }
+            
         }
         public void OnGet()
         {
@@ -56,8 +36,37 @@ namespace CharityDatabase.Pages.Needee
                 }
                 finally
                 {
-                    _context.Dispose();
+                    //_context.Dispose();
                 }
+            }
+        }
+        public void OnPost()
+        {
+            string id = Request.Query["Id"];
+            if (!string.IsNullOrEmpty(id))
+            {
+                int idToEdit = Convert.ToInt32(id);
+                needeeToEdit = _context.Needees.FirstOrDefault(n => n.Id == idToEdit);
+
+            }
+            try
+            {
+                if (needeeToEdit != null)
+                {
+                    needeeToEdit.FirstName = Request.Form["FirstNameTextBox"].ToString();
+                    needeeToEdit.LastName = Request.Form["LastNameTextBox"].ToString();
+                    needeeToEdit.NationalCode = Request.Form["NationalCodeTextBox"].ToString();
+                    _context.SaveChanges();
+                    Response.Redirect("Needees");
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewData["Error"] = ex.Message;
+            }
+            finally
+            {
+                _context.Dispose();
             }
         }
     }
--- CharityDatabase/Pages/Needee/Needees.cshtml.cs
+++ CharityDatabase/Pages/Needee/Needees.cshtml.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -18,6 +18,38 @@ namespace CharityDatabase.Pages
         }
         public void OnGet()
         {
+        }
+        public void OnPost()
+        {
+            string searchTerm = Request.Form["SearchTextBox"].ToString();
+            try
+            {
+                needees = search(searchTerm);
+            }
+            catch(Exception ex)
+            {
+                ViewData["Error"] += ex.Message;
+            }
+        }
+        public List<Db.Needee> search(string searchTerm)
+        {
+            if (_context != null)
+            {
+                try
+                {
+                    needees = _context.Needees.Where(n => n.FirstName.Contains(searchTerm) ||
+                    n.LastName.Contains(searchTerm) || n.NationalCode.Contains(searchTerm)).ToList();
+                }
+                catch(Exception ex)
+                {
+                    throw new Exception("جستجو در پایگاه داده با خطا مواجه شد");
+                }
+            }
+            else
+            {
+                throw new Exception("ارتباط با پایگاه داده میسر نشد");
+            }
+            return needees;
         }
     }
 }
